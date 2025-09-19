3f67aaada3c40dd86fe639c2e22f44292af9402c
leonardotorricoIT <leonardo.torrico@assuresoft.tech>
2025-09-19 02:43:39 -0400

    feat: add  topics mock

---
+++ src/Data/mock.ts
@@ -0,0 +1,7 @@
+import type { Topic } from "../types/Flashcard";
+
+export const topics: Topic[] = [
+  { id: "1", name: "Math", color: "bg-retro-yellow" },
+  { id: "2", name: "History", color: "bg-retro-pink" },
+  { id: "3", name: "Programming", color: "bg-retro-blue" },
+];
